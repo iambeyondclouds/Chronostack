8c04f5b1beff115d88d54fbd24b474fac993e402
Aashita Srivastava <beyondclouds6340@gmail.com>
2026-03-15 21:17:27 +0530

    Add replay timeline support for inspecting state evolution

--- core/replayEngine.js
+++ core/replayEngine.js
@@ -11,6 +11,22 @@ class ReplayEngine {
     const filteredEvents = events.filter(event => event.version <= version);
     return this.stateEngine.reconstruct(filteredEvents);
   }
+
+  getReplayTimeline(events) {
+    let timeline = [];
+    let state = this.stateEngine.initialState;
+
+    for (const event of events) {
+      state = this.stateEngine.reducer(state, event);
+
+      timeline.push({
+        version: event.version,
+        state: state
+      });
+    }
+
+    return timeline;
+  }
 }
 
 module.exports = ReplayEngine;
