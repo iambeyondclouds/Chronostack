fe41334ab7edc995e9627f156f2593185e54c92f
Aashita Srivastava <beyondclouds6340@gmail.com>
2026-03-29 03:52:08 +0530

    Connenct frontend to backend API for real-time data display

--- chronostack-ui/src/app/page.js
+++ chronostack-ui/src/app/page.js
@@ -1,46 +1,58 @@
-const timeline = [
-  { version: 1, count: 5 },
-  { version: 2, count: 8 },
-  { version: 3, count: 6 }
-];
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/data")
+      .then(res => res.json())
+      .then(data => setData(data))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <div style={{ padding: "30px", fontFamily: "Arial", lineHeight: "1.6" }}>
       <h1 style={{ marginBottom: "20px" }}>ChronoStack Dashboard</h1>
 
+      {/* Final State */}
       <h2 style={{ marginTop: "20px" }}>Final State</h2>
-      <p>{timeline[timeline.length - 1].count}</p>
+      <p>{data ? data.finalState.count : "Loading..."}</p>
 
+      {/* Timeline */}
       <h2 style={{ marginTop: "20px" }}>Timeline</h2>
       <ul>
-        {timeline.map(step => (
-          <li key={step.version}>
-            Version {step.version} {"->"} {step.count}
-          </li>
-        ))}
+        {data
+          ? data.timeline.map(step => (
+              <li key={step.version}>
+                Version {step.version} {"->"} {step.state.count}
+              </li>
+            ))
+          : "Loading..."}
       </ul>
 
-      
+      {/* State Diff */}
       <h2 style={{ marginTop: "20px" }}>State Diff</h2>
       <ul>
-        {timeline.slice(1).map((step, i) => {
-          const prev = timeline[i].count;
-          const diff = step.count - prev;
+        {data &&
+          data.timeline.slice(1).map((step, i) => {
+            const prev = data.timeline[i].state.count;
+            const diff = step.state.count - prev;
 
-          return (
-            <li key={step.version}>
-              Version {step.version}: {prev} {"->"} {step.count} (Δ {diff})
-            </li>
-          );
-        })}
+            return (
+              <li key={step.version}>
+                Version {step.version}: {prev} {"->"} {step.state.count} (Δ {diff})
+              </li>
+            );
+          })}
       </ul>
 
+      {/*casuality*/} 
       <h2 style={{ marginTop: "20px" }}>Causality</h2>
-<ul>
-  <li>Event 1 → Event 2</li>
-  <li>Event 2 → Event 3</li>
-</ul>
+      <ul>
+        <li>Event 1 → Event 2</li>
+        <li>Event 2 → Event 3</li>
+      </ul>
     </div>
   );
 }
